9878e9c081bdf302ec741924b86fbe01081715b9
Robert Orzanna <orschiro@gmail.com>
2015-01-21 17:29:47 +0100

    copyq fetch image from /tmp/screenshot

--- Scrotclip/scrotclip.sh
+++ Scrotclip/scrotclip.sh
@@ -8,6 +8,6 @@
 
 notify-send -i warning "Taking Screenshot" "Please select your area" --icon=camera-photo
 maim -s -c 1,0,0,0.6 -p 10 -d 5 /tmp/screenshot.png
-copyq write image/png - < ~/screenshot.png
+copyq write image/png - < /tmp/screenshot.png
 copyq select 0
 notify-send -i warning "Taking Screenshot" "Screenshot copied to clipboard" --icon=camera-photo
