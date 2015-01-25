2d1b5181e4f6ef02443dfaaf85138d8395f239cf
Robert Orzanna <orschiro@gmail.com>
2015-01-25 08:43:12 +0100

    keep notification until task is done

--- ConkyTimer/conkyTimer.sh
+++ ConkyTimer/conkyTimer.sh
@@ -9,16 +9,19 @@ trap "rm ~/.conkytimer" INT
 
 task=$1
 min=$2
+millisecondsToMinute=$(( 60000*$2 ))
+
+notify-send -t $millisecondsToMinute -i warning "Starting Timer" "$task" --icon=appointment-soon
 
 for (( i = 0; i < $min; i++ )); do
-	# write remaining min to file
-	timer=$((min-i))
-	display="${timer}min: $task"
-	echo "$display" > ~/.conkytimer
-	# wait 1min
-	sleep 60
+        # write remaining min to file
+        timer=$((min-i))
+        display="${timer}min: $task"
+        echo "$display" > ~/.conkytimer
+        # wait 1min
+        sleep 60
 done
 
-rm ~/.conkytimer 
-notify-send -t 100000 -i warning "Finished Timer" "$task" --icon=appointment-soon
+rm ~/.conkytimer
+notify-send -i warning "Finished Timer" "$task" --icon=appointment-soon
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga
